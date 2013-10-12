8fc634793d557d6ae07bddc74a49c474786cc3e2
Łukasz Wieczorek <lukasz.wieczorek@appunite.com>
2013-10-12 19:09:41 +0200

    initial

---
+++ copy.rb
@@ -0,0 +1,24 @@
+require 'fileutils'
+require 'nokogiri'
+
+if ARGV.count != 2 then
+  puts "Wrong number of arguments\n\t./ruby copy.rb xspf-file destination"
+  exit
+end
+
+src = ARGV[0]
+dest = ARGV[1]
+
+doc = Nokogiri.XML(open(src))
+collection = doc.xpath('//ns:location', ns: 'http://xspf.org/ns/0/')
+collection_size = collection.count
+i = 1
+collection.each do |location|
+  print "\r#{i}/#{collection_size}"
+  file_path = location.content
+  unless File.new(file_path).exists?
+    FileUtils.copy(file_path, dest)
+  end
+  i = i + 1
+end
+puts
